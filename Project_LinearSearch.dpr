24a8a676404f86cb77585d0840100bc318322b15
Chagas Filho <chagas_filho@yahoo.com.br>
2016-05-23 11:23:18 -0300

    Initial Commit

---
+++ Project_LinearSearch.dpr
@@ -0,0 +1,24 @@
+program Project_LinearSearch;
+const
+  Linha: array[1..20] of integer = (20, 16, 23, 05, 10, 19, 83, 32, 33, 52, 04, 55, 15, 07, 02, 98, 65, 43, 71, 87);
+var
+  Key: integer;
+
+function LinearSearch(Key: integer; Linha: array of integer): integer;
+var
+  i: Integer;
+begin
+  for i := 0 to 19 do
+    begin
+      if linha[i] = Key then Result := i else Result := -1;
+    end;
+end;
+
+begin
+  Writeln('20 16 23 05 10 19 83 32 33 52 04 55 15 07 02 98 65 43 71 87');
+  Readln;
+  Writeln('Informe um Número da Sequência a cima: ');
+  Readln(Key);
+  Writeln('A posição do valor ', Key, ' está no índice: ', LinearSearch(Key, Linha));
+  Readln;
+end.
