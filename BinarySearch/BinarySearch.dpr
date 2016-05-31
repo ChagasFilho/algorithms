f27440b70331d820dce3a0c493bbd07ea9799569
Chagas Filho <chagas_filho@yahoo.com.br>
2016-05-30 21:38:40 -0300

    LinearSearsh, BinarySearch and InsertionSort Algorithm.

---
+++ BinarySearch/BinarySearch.dpr
@@ -0,0 +1,38 @@
+program BinarySearch;
+const
+  Linha: array[1..20] of integer = (02, 04, 05, 07, 10, 15, 16, 19, 20, 23, 32, 33, 43, 52, 55, 65, 71, 83, 87, 98);
+var
+  Key: integer;
+
+  function BSearch(Key: integer; Linha: array of integer): integer;
+  var
+    Esquerda, Meio, Direita: Integer;
+  begin
+
+    Esquerda := 0;
+    Direita := 19;
+
+    Result := -1;
+
+    while (Esquerda <= Direita) and (Result = -1) do
+      begin
+        Meio := (Esquerda + Direita) div 2;
+
+        if (Key = Linha[Meio]) then
+          Result := Meio
+        else if (Key < Linha[Meio]) Then
+          Direita := Meio - 1
+        else
+          Esquerda := Meio + 1;
+      end;
+
+  end;
+
+begin
+  Writeln('02, 04, 05, 07, 10, 15, 16, 19, 20, 23, 32, 33, 43, 52, 55, 65, 71, 83, 87, 98');
+  Read;
+  Writeln('Informe um Número da Sequência a cima: ');
+  Readln(Key);
+  writeln('A posição do valor ', Key, ' está no índice: ', BSearch(Key, Linha));
+  Readln;
+end.
